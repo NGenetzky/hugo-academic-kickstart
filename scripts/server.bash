e1f1149fbbc845947b18597c84a63a2b59489da0
Nathan Genetzky <nathan@genetzky.us>
2018-10-22 23:49:31 -0500

    scripts: created 'server.bash'

---
+++ scripts/server.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPTDIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
+
+server()
+{
+  cd "${SCRIPTDIR}/../"
+  hugo serve
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    set -x
+    server "$@"
+fi

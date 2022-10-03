b4fed58aed2e90297d0bcc046353d3f9f6739529
Nathan Genetzky <nathan@genetzky.us>
2022-10-03 05:42:54 +0000

    script: cibuild and env based on STRTA
    script: Add 'env'

--- script/cibuild.bash
+++ script/cibuild.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 _cibuild(){
+  hugo version
+
   _DEFAULT_URL="http://127.0.0.1:1313/"
   URL="${URL-"${_DEFAULT_URL}"}"
-
   hugo --gc --minify -b $URL
+
+  # hugo --cacheDir "$(pwd)/.cache/"
+
+  # hugo -c content -d public --configDir config --themesDir themes "$@"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then

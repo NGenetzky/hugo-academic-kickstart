064cc62be87dbfdeeb2d68ae0d0284be2bd64b52
Nathan Genetzky <nathan@genetzky.us>
2022-11-25 11:49:13 -0600

    script: update_hugo script to automate version updates

---
+++ script/update_hugo.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+update_hugo(){
+  sed -i \
+    -e "s@${HUGO_VERSION_OLD?}@${HUGO_VERSION_NEW?}@" \
+    ./.devcontainer/devcontainer.json \
+    ./docker-compose.yml \
+    ./netlify.toml \
+    ./script/bootstrap.bash \
+    ./script/env
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  # Bash Strict Mode
+  set -eu -o pipefail
+
+  SCRIPTDIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
+
+  . script/env
+
+  HUGO_VERSION_OLD="${HUGO_VERSION}"
+  HUGO_VERSION_NEW="${1?Please specify HUGO_VERSION_NEW}"
+
+  update_hugo
+fi
